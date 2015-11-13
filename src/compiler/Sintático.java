1fd360d5715de9fa27e17bdb64c85ac503c4c778
'Loraine <'Loraine@192.168.25.208>
2015-11-13 13:58:54 -0200

    Classe Sintatico Alterada, tem um erro na função Avanca(), fiquei na duvida. Falta implementar as verificações.

--- src/compiler/Sintático.java
+++ src/compiler/Sintático.java
@@ -21,8 +21,40 @@ import java.util.LinkedList;
  * @author 'Loraine
  */
 public class Sintático {
+     private Lexer lexer;
+    private Token token;
+    Hashtable<String, Token> sint;
+    int i=0;//para o for que vai rodar a hashtable
     
-    public void verifica(Hashtable<String, Token> words)
+    //implementação baseada no livro
+    public Sintático(Lexer lexer) throws IOException {
+        this.lexer = lexer;
+        token = lexer.scan();
+    }
+    
+    private void avanca() throws IOException {
+        //DUVIDA
+        //teria que fazer um for pra pegar o proximo token da lista de words, então fiz uma variavel I global que vai fazendo isso
+        //MELHOR CONFERIR   
+        token = lexer.words.elements(i);
+        i++;
+    }
+    
+    private void SintaticoErro() {
+        System.out.println("Erro  na linha "+Lexer.line+" perto do token "+token.toString());
+        System.exit(0);
+    }
+    
+    private void eat(int tag) throws IOException {
+        if (token.tag == tag){
+            System.out.println("eat " +token);
+            avanca();
+        }
+        else
+            SintaticoErro();
+    }
+    
+    public void verifica()
     {
         
     }
