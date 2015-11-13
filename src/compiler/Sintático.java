ef1d72bb3d450d711e5e3935aa2b845aab184beb
'Loraine <'Loraine@192.168.25.208>
2015-11-12 23:10:00 -0200

    add classe sintatico

---
+++ src/compiler/Sintático.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package compiler;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Stack;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author 'Loraine
+ */
+public class Sintático {
+    
+    public void verifica(Hashtable<String, Token> words)
+    {
+        
+    }
+    
+}
